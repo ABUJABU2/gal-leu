c2a54a036d37afec0a543dc861bb5a7a3cf397aa
ABUJABU2 <brandojoao86@gmail.com>
2025-06-05 17:10:40 -0300

    Add files via upload

---
+++ lacos.py
@@ -0,0 +1,18 @@
+#CRIE UMA VARIAVEL CHAMADA SOMA
+#logo em seguida some o numero 1 no valor antigo dela
+soma=0
+
+for i in range(10): #para cada i dentro do intervalo ate 10 quero q rode o segunte codigo
+    soma = soma+1
+    print(soma)
+
+#while True:
+#   print('o Vinicius mora numa caixa')
+
+soma=0
+while soma<1000: #enquanto a variavel soma for menor q 1000
+    soma=soma+10 #quero q some 10 do valor antigo
+    print(soma) #logo em seguida, quero q imprima o resultado em tela
+#10
+#20
+#30...
