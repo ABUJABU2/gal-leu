2883aa31b8cb2a0f5b8a8d8218442fead0ce0b68
ABUJABU2 <brandojoao86@gmail.com>
2025-06-24 17:35:14 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,12 @@
+#listas
+
+#analise de media de notas
+lista_de_media_notas = [1.5,0.1,1.6,2.0]
+
+#comando sum(), soma os elementos de uma lista
+#comando len(), retoma o numero de itens de uma lista
+media = sum(lista_de_media_notas) / len(lista_de_media_notas)
+print(media)
+lista_nomes = ['vitor','vini','abujabu2','onix']
+print(lista_nomes[-2])
+print(lista_nomes[-1])
