8fa1f02b06c9cbf56becd622c87c7896e90bc5dc
ABUJABU2 <brandojoao86@gmail.com>
2025-06-26 17:24:39 -0300

    Add files via upload

---
+++ listanalista.py
@@ -0,0 +1,12 @@
+#--------------------------------------------------------LISTAS NAS LISTAS------------------------------------------------------------------------------------
+lista_toletao = [
+    ['alvaro', 'alvaro.sampaio@galileunegocios.com.br', 32],
+    ['felipe', 'felipe.nasciben@galileunegocios.com.br', 41],
+    ['matheus', 'matheus.biasotto@galileunegocios.com.br', 29]
+]
+
+#para abrir uma lista dentro de uma lista coloque o numero da lista e o numero do q voce quer acessar na lista: print(lista_toletao[1][2])
+
+print(lista_toletao[1][2])
+print(lista_toletao[2][1])
+print(lista_toletao[0][1])
