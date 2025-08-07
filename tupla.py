92a64d91d81df46ea2e2800a2ff22b3a82447c85
ABUJABU2 <brandojoao86@gmail.com>
2025-08-07 16:49:53 -0300

    Add files via upload

---
+++ tupla.py
@@ -0,0 +1,10 @@
+nomes = ('Alvaro',
+         'leticia',
+         'bruno',
+         'henrique',
+         'carlos')
+
+print(nomes[1:4]) #leticia,bruno,henrique#nome da tupla[inicio:final+1passo]
+print(nomes[0:3])#alvaro,leticia,bruno
+print(nomes[0::2])#alvaro,bruno,carlos
+print(nomes[::-1])#todo mundo au contrario
